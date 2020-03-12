b6cd81fa81477f93dd686decb7a1843ddb67f689
SandeepKandagatla <59524137+SandeepKandagatla@users.noreply.github.com>
2020-03-12 14:41:24 +0530

    Add files via upload

---
+++ ImplementHttpsandHSTS/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ImplementHttpsandHSTS
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddHsts((hstsOptions) =>
+            {
+                hstsOptions.IncludeSubDomains = true;
+                hstsOptions.MaxAge = TimeSpan.FromMinutes(5);
+                hstsOptions.ExcludedHosts.Clear();//demo only
+            });
+            services.AddHttpsRedirection((httpOptions) =>
+            {
+                //httpOptions.RedirectStatusCode = 307;
+                //httpOptions.HttpsPort = 443;
+
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseHsts();//dont keep here in prod, just to understand hsts
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+               // app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
